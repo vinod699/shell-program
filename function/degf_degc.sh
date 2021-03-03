5634f5ee75ed12293934faffbf87092e6f621ff5
vinod699 <kosarajuvinod9999@gmail.com>
2021-03-03 09:23:17 +0530

    degf_degc.sh

---
+++ function/degf_degc.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -x
+
+function degree ()
+{
+degc=$1
+degf=$2
+echo "Fahrenheit to Celsius"
+degf=$(( (degc-32)*5/9 ))
+echo "Faherenhiet =" $degf
+echo
+echo "Celsius to Fahrenheit"
+degc=$(( (degf*9/5)+32 ))
+echo "Celsius =" $degc
+}
+read -p "Degree in Celsius= " degc
+read -p "Degree in Fahreheit= " degf
+degree $degc $degf
